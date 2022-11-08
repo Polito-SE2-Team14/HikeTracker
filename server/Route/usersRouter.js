94515a267f2cac7754d506fc3859a3e60105d733
Edoardo Colella s257113 <s257113@studenti.polito.it>
2022-11-09 00:02:55 +0100

    validation in userDao

--- server/Route/usersRouter.js
+++ server/Route/usersRouter.js
@@ -3,20 +3,23 @@ const router = express.Router()
 const prefixRoute = '/api/';
 const isLoggedIn = router.get('isLoggedIn')
 const userDAO = require('../DAO/UserDAO')
+const { body, validationResult } = require('express-validator');
 
-router.post(prefixRoute + 'users', async (req, res) => {
 
-    const name = req.body.name;
-    const surname = req.body.surname;
-    const email = req.body.email;
-    const phoneNumber = req.body.phoneNumber;
-    const type = req.body.type;
-    const password = req.body.password;
+router.post(prefixRoute + 'users',
+    body("name").not().isEmpty().trim().escape(),
+    body("surname").not().isEmpty().trim().escape(),
+    body('email').isEmail().normalizeEmail(),
+    body("phoneNumber").not().isEmpty().trim().escape(),
+    body("type").not().isEmpty().trim().escape(),
+    body("password").not().isEmpty().trim().escape(),
+    async (req, res) => {
 
-    console.log(name,surname, email, phoneNumber, type, password)
-
-    await userDAO.Register(name, surname, email, phoneNumber, type, password)
-        .then(() => res.status(201).end())
-        .catch(err => res.status(505).send("error"))
-});
+        if (validationResult(req).isEmpty() && (req.body.type === "hiker" || req.body.type === "friend")) {
+            await userDAO.Register(req.body.name, req.body.surname, req.body.email, req.body.phoneNumber, req.body.type, req.body.password)
+                .then(() => res.status(201).end())
+                .catch(err => res.status(505).send("error"))
+        }
+        else return res.status(505).json(errors.array())
+    });
 module.exports = router;
