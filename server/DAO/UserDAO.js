27ea579361f0094c1216802e71ab5870a2fb490f
LurkingNemo <giovanniriela99@gmail.com>
2022-11-08 16:27:38 +0100

    Code refactoring (implemeted User object in UserDAO)

--- server/DAO/UserDAO.js
+++ server/DAO/UserDAO.js
@@ -1,8 +1,10 @@
-const { db } = require("./dbManager");
+const { db } = require("../database/dbManager");
 const crypto = require("crypto");
 
+const User = require("../Class/User");
+
 function CheckExisting(email, phoneNumber) {
-	return new Promise((resolve, reject) =>{
+	return new Promise((resolve, reject) => {
 		let sql = "SELECT COUNT(*) as N FROM User WHERE email = ? OR phoneNumber = ?";
 
 		db.get(sql, [email, phoneNumber], (row, err) => {
@@ -32,22 +34,22 @@ function EncryptPassword(password) {
 	})
 }
 
-exports.getUserById=(id)=>{
-	return new Promise((resolve,reject)=>{
-		const sql='SELECT * FROM USER WHERE userID=?';
-		db.get(sql,[id],(err,row)=>{
+exports.getUserById = (id) => {
+	return new Promise((resolve, reject) => {
+		const sql = 'SELECT * FROM USER WHERE userID=?';
+		db.get(sql, [id], (err, row) => {
 			if (err)
 				reject(err);
-			else if (row===undefined)
-				resolve({error: 'User not found'});
-			else{
-				const user={userID:row.userID, name:row.name, surname:row.surname, phoneNumber:row.phoneNumber, type: row.type}
+			else if (row === undefined)
+				resolve({ error: 'User not found' });
+			else {
+				const user = { userID: row.userID, name: row.name, surname: row.surname, phoneNumber: row.phoneNumber, type: row.type }
 				resolve(user);
 			}
 		});
 	});
 };
-	
+
 exports.getUser = (email, password) => {
 	return new Promise((resolve, reject) => {
 		const sql = 'SELECT * FROM USER WHERE email = ?';
@@ -55,13 +57,13 @@ exports.getUser = (email, password) => {
 			if (err) { reject(err); }
 			else if (row === undefined) { resolve(false); }
 			else {
-				const user = {userID:row.userID, name:row.name, surname:row.surname, phoneNumber:row.phoneNumber, type: row.type};
-		  
+				const user = { userID: row.userID, name: row.name, surname: row.surname, phoneNumber: row.phoneNumber, type: row.type };
+
 				const salt = row.salt;
 				crypto.scrypt(password, salt, 16, (err, hashedPassword) => {
 					if (err) reject(err);
 					const passwordHex = Buffer.from(row.hash, 'hex');
-					if(!crypto.timingSafeEqual(passwordHex, hashedPassword))
+					if (!crypto.timingSafeEqual(passwordHex, hashedPassword))
 						resolve(false);
 					else resolve(user);
 				});
@@ -79,18 +81,18 @@ exports.getUser = (email, password) => {
  * @param {number} phoneNumber 
  * @param {string} type 
  * @param {string} password 
- * @returns TBD
+ * @returns User object
  */
 exports.Register = (name, surname, email, phoneNumber, type, password) =>
 	CheckExisting(email, phoneNumber).then(() =>
-	
-	EncryptPassword(password)).then((pass) =>
-	
-	new Promise((resolve, reject) => {
-		let sql = "INSERT INTO User(NAME, SURNAME, EMAIL, PHONENUMBER, TYPE, SALT, HASHEDPASSWORD) VALUES(?, ?, ?, ?, ?, ?, ?)";
 
-		db.run(sql, [name, surname, email, phoneNumber, type, pass.salt, pass.hashedPassword], err => {
-			if (err) reject();
-			else resolve();
-		})
-	}))
+		EncryptPassword(password)).then((pass) =>
+
+			new Promise((resolve, reject) => {
+				let sql = "INSERT INTO User(NAME, SURNAME, EMAIL, PHONENUMBER, TYPE, SALT, HASHEDPASSWORD) VALUES(?, ?, ?, ?, ?, ?, ?)";
+
+				db.run(sql, [name, surname, email, phoneNumber, type, pass.salt, pass.hashedPassword], function (err) {
+					if (err) reject();
+					else resolve(new User(this.lastID, name, surname, email, phoneNumber, type));
+				})
+			}))
