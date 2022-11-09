a46709da8e8b8bc349336061778845325f7370d9
LucaBasso <s258211@studenti.polito.it>
2022-11-09 16:24:26 +0100

    Added test files

--- server/test/APICalls/hikeAPICalls.js
+++ server/test/APICalls/hikeAPICalls.js
@@ -2,73 +2,73 @@
 
 const axios = require('axios');
 
-class GenericAPICall {
-    #baseURL;
+class hikeCall {
+	#baseURL;
 
-    constructor() {
-        this.#baseURL = "http://localhost:3001";
-    }
+	constructor() {
+		this.#baseURL = "http://localhost:3001";
+	}
 
-    //GET
-    async getMethod() {
-        const url = this.#baseURL + "/api/genericGet";
-        let response;
+	//GET
+	async getHikes() {
+		const url = this.#baseURL + "/api/hikes";
+		let response;
 
-        await axios.get(url)
-            .then(value => response = value)
-            .catch(error => response = error.response);
+		await axios.get(url)
+			.then(value => response = value)
+			.catch(error => response = error.response);
 
-        return response;
-    }
+		return response;
+	}
 
 
-    //POST
-    async postMethod(param1, param2, param3) {
-        const url = this.#baseURL + "/api/genericPost";
-        const body = {
-            param1: param1,
-            param2: param2,
-            param3: param3
-        }
-        const headers = { headers: { 'Content-Type': 'application/json' } };
-        let response;
+	//POST
+	async postMethod(param1, param2, param3) {
+		const url = this.#baseURL + "/api/genericPost";
+		const body = {
+			param1: param1,
+			param2: param2,
+			param3: param3
+		}
+		const headers = { headers: { 'Content-Type': 'application/json' } };
+		let response;
 
-        await axios.post(url, body, headers)
-            .then(value => response = value)
-            .catch(error => response = error.response);
+		await axios.post(url, body, headers)
+			.then(value => response = value)
+			.catch(error => response = error.response);
 
-        return response;
-    }
+		return response;
+	}
 
-    //PUT
-    async putMethod(param1, param2, param3) {
-        const url = this.#baseURL + "/api/genericPut";
-        const body = {
-            param1: param1,
-            param2: param2,
-            param3: param3
-        }
-        const headers = { headers: { 'Content-Type': 'application/json' } };
-        let response;
+	//PUT
+	async putMethod(param1, param2, param3) {
+		const url = this.#baseURL + "/api/genericPut";
+		const body = {
+			param1: param1,
+			param2: param2,
+			param3: param3
+		}
+		const headers = { headers: { 'Content-Type': 'application/json' } };
+		let response;
 
-        await axios.put(url, body, headers)
-            .then(value => response = value)
-            .catch(error => response = error.response);
+		await axios.put(url, body, headers)
+			.then(value => response = value)
+			.catch(error => response = error.response);
 
-        return response;
-    }
+		return response;
+	}
 
-    //DELETE
-    async deleteMethod(param) {
-        const url = this.#baseURL + "/api/genericDelete/" + param;
-        let response;
+	//DELETE
+	async deleteMethod(param) {
+		const url = this.#baseURL + "/api/genericDelete/" + param;
+		let response;
 
-        await axios.delete(url)
-            .then(value => response = value)
-            .catch(error => response = error.response);
+		await axios.delete(url)
+			.then(value => response = value)
+			.catch(error => response = error.response);
 
-        return response;
-    }
+		return response;
+	}
 
 }
 
--- server/test/hikeTest.js
+++ server/test/hikeTest.js
@@ -6,7 +6,7 @@ const expect = chai.expect;
 const should = chai.should();
 const axios = require('axios');
 
-const GenericAPICall = require('./APICalls/GenericCalls');
+const hikeAPICall = require('./APICalls/hikeAPICalls');
 
 
 const baseURL = "http://localhost:3001";
@@ -15,30 +15,47 @@ const dbmanager = new DBManager()
 const genericAPICall = new GenericAPICall();
 
 
+describe('Hikes test suite', async () => {
+	beforeEach(async () => {
+		await dbmanager.restoreOriginalData();
+	})
+	after(async () => {
+		await dbmanager.restoreOriginalData();
+	})
+
+	describe('Get all hikes', async () => {
+		it('a single test', async () => {
+			const response = await hikeAPICall.getHikes()
+			assert.equal(response.status, 200, response.status);
+		})
+	})
+
+});
+
 describe('generic test suite', async () => {
 
-    beforeEach(async () => {
-        await dbmanager.deleteAllData();
-    })
-    after(async () => {
-        await dbmanager.deleteAllData();
-    })
+	beforeEach(async () => {
+		await dbmanager.deleteAllData();
+	})
+	after(async () => {
+		await dbmanager.deleteAllData();
+	})
 
   
-    describe('things that work', async () => {
-        it('a single test', async () => {
-            const response = await genericAPICall.genericMethod();
-            assert.equal(response.status, 200, response.status);
-        })
-    })
+	describe('things that work', async () => {
+		it('a single test', async () => {
+			const response = await hikeAPICall.getHike()
+			assert.equal(response.status, 200, response.status);
+		})
+	})
 
-    describe('things that dont work', async () => {
+	describe('things that dont work', async () => {
 
-        it('a single test', async () => {
-            const response = await genericAPICall.genericMethod();
-            assert.equal(response.status, 500, response.status);
-        })
+		it('a single test', async () => {
+			const response = await genericAPICall.genericMethod();
+			assert.equal(response.status, 500, response.status);
+		})
 
-    })
+	})
 
 });
