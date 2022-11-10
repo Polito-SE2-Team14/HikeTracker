5065669256895123e3fca98cb8a6adcc666ad447
Edoardo Colella s257113 <s257113@studenti.polito.it>
2022-11-10 13:10:02 +0100

    changed name

---
+++ server/database/DBManagerSingleton.js
@@ -0,0 +1,14 @@
+
+class Singleton{
+
+    static #instance = null;
+    
+    static getInstance(){
+        if(this.#instance == null){
+            this.#instance = new Controller()
+        }
+        return this.#instance;
+    }
+}
+
+module.exports = Singleton
