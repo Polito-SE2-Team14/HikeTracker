b246288069bca801292528372a0e4f48d9c6c408
Edoardo Colella <edoardocolella.it@gmail.com>
2022-11-10 10:44:50 +0100

    used backticks

--- server/database/dbManager.js
+++ server/database/dbManager.js
@@ -29,10 +29,10 @@ exports.restoreOriginalHikes = async () => {
 	let db=this.db;
 	return new Promise(function (resolve,reject){
 		db.run("DELETE FROM HIKE WHERE 1=1;")
-		db.run("INSERT INTO HIKE (hikeID, title, lenght, expectedTime, ascent, difficulty, startPointID, endPointID, description) VALUES \
-			(1, \"hike#1\", 7, 30, 100, \"Tourist\", 1, 4, \"firstDescription\"), \
-			(2, \"hike#2\", 2, 45, 123, \"Hiker\", 2, 5, \"secondDescription\"), \
-			(3, \"hike#3\", 3, 60, 514, \"Professional Hiker\", 3, 6, \"thirdDescription\");");
+		db.run(`INSERT INTO HIKE (hikeID, title, lenght, expectedTime, ascent, difficulty, startPointID, endPointID, description) VALUES 
+			(1, "hike#1", 7, 30, 100, "Tourist", 1, 4, "firstDescription"), 
+			(2, "hike#2", 2, 45, 123, "Hiker", 2, 5, "secondDescription"), 
+			(3, "hike#3", 3, 60, 514, "Professional Hiker", 3, 6, "thirdDescription");`);
 		resolve();
 	})
 }
