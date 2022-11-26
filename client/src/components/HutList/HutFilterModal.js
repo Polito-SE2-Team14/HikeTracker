4eb6827469ad48bea0977d90cd8a48495169d2b3
Antonio Campobasso <campobassoantonio7@gmail.com>
2022-11-26 18:10:22 +0100

    hut location update

--- client/src/components/HutList/HutFilterModal.js
+++ client/src/components/HutList/HutFilterModal.js
@@ -3,10 +3,13 @@ import { Button, Modal, Form } from "react-bootstrap";
 import { AreaSelectMap } from "../../components/Map/Maps";
 
 export function HutFilterModal(props) {
+	const [showProvinceForm, setShowProvinceForm] = useState(false);
 	const [showLocationForm, setShowLocationForm] = useState(false);
 	const [showAddressForm, setShowAddressForm] = useState(false);
 	const [showBedspaceForm, setShowBedspaceForm] = useState(false);
 
+	const [province, setProvince] = useState("");
+	const [municipality, setMunicipality] = useState("");
 	const [area, setArea] = useState({});
 	const [address, setAddress] = useState("");
 	const [minBedspace, setMinBedspace] = useState(0);
@@ -17,6 +20,8 @@ export function HutFilterModal(props) {
 		// TODO(antonio): add validation
 		props.setFilters({
 			...props.filters,
+			province: showProvinceForm ? province : "",
+			municipality: showProvinceForm ? municipality : "",
 			area: showLocationForm ? area : {},
 			address: showAddressForm ? address : "",
 			bedspace: showBedspaceForm ? minBedspace : 0,
@@ -34,15 +39,43 @@ export function HutFilterModal(props) {
 					<span className="d-flex">
 						<Form.Check
 							type="switch"
+							checked={showProvinceForm}
+							onChange={(e) => setShowProvinceForm(e.target.checked)}
+						/>
+						<Form.Label>Province filter</Form.Label>
+					</span>
+					{showProvinceForm ? (
+						<>
+							<Form.Control
+								placeholder="Insert province"
+								value={province}
+								onChange={(e) => setProvince(e.target.value)}
+							/>
+							<Form.Text>Province</Form.Text>
+							<Form.Control
+								placeholder="Insert municipality"
+								value={municipality}
+								onChange={(e) => setMunicipality(e.target.value)}
+							/>
+							<Form.Text>Municipality</Form.Text>
+						</>
+					) : (
+						false
+					)}
+					<span className="d-flex">
+						<Form.Check
+							type="switch"
 							checked={showLocationForm}
-							onChange={(e) => setShowLocationForm(e.target.checked)} />
+							onChange={(e) => setShowLocationForm(e.target.checked)}
+						/>
 						<Form.Label>Area filter</Form.Label>
 					</span>
 					{showLocationForm ? (
 						<AreaSelectMap
 							onSetArea={(area) => {
 								setArea(area);
-							}} />
+							}}
+						/>
 					) : (
 						false
 					)}
@@ -50,14 +83,16 @@ export function HutFilterModal(props) {
 						<Form.Check
 							type="switch"
 							checked={showAddressForm}
-							onChange={(e) => setShowAddressForm(e.target.checked)} />
+							onChange={(e) => setShowAddressForm(e.target.checked)}
+						/>
 						<Form.Label>Address filter</Form.Label>
 					</span>
 					{showAddressForm ? (
 						<Form.Control
 							placeholder="Insert address"
 							value={address}
-							onChange={(e) => setAddress(e.target.value)} />
+							onChange={(e) => setAddress(e.target.value)}
+						/>
 					) : (
 						false
 					)}
@@ -65,7 +100,8 @@ export function HutFilterModal(props) {
 						<Form.Check
 							type="switch"
 							checked={showBedspaceForm}
-							onChange={(e) => setShowBedspaceForm(e.target.checked)} />
+							onChange={(e) => setShowBedspaceForm(e.target.checked)}
+						/>
 						<Form.Label>Bedspace filter</Form.Label>
 					</span>
 					{showBedspaceForm ? (
@@ -74,7 +110,8 @@ export function HutFilterModal(props) {
 								type="number"
 								placeholder="Insert bedspace"
 								value={minBedspace}
-								onChange={(e) => setMinBedspace(e.target.value)} />
+								onChange={(e) => setMinBedspace(e.target.value)}
+							/>
 							<Form.Text>Minimum bedspace available</Form.Text>
 						</>
 					) : (
