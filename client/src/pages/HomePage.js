841bf3815e6f3904dad9af1f25d00f288fbaaeb2
LurkingNemo <giovanniriela99@gmail.com>
2022-12-11 18:08:19 +0100

    added empty message for new user suggestion

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -3,60 +3,63 @@ import { Navigate } from "react-router-dom";
 
 import { Loading } from "../components/Loading";
 import HikeListTable from "../components/HikeList/HikeListTable";
-import { filterAllHikes } from "../components/HikeList/filtering_functions";
 import HikeAPI from "../api/HikeAPI";
 import UserAPI from "../api/UserAPI";
-import { Container } from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
 
 export function HomePage(props) {
 	const [loading, setLoading] = useState(true);
+	const [userStats, setUserStats] = useState(null);
 	const [hikes, setHikes] = useState([]);
 	const [selectedHike, setSelectedHike] = useState(null);
-	const [filteredHikes, setFilteredHikes] = useState([]);
 
 	const getSuggestedHikes = async () => {
 		let stats = await UserAPI.getUserStats();
 		let hikes
 		await HikeAPI.getAllHikes()
 			.then(h => {
-				hikes = h;
+				if (stats) {
+					hikes = h;
 
-				let suggested = hikes.filter(hike =>
-					hike.difficulty === stats.favouriteDifficulty
-				);
+					let suggested = hikes.filter(hike =>
+						hike.difficulty === stats.favouriteDifficulty
+					);
 
-				if (suggested.length > 0) hikes = suggested;
+					if (suggested.length > 0) hikes = suggested;
 
-				suggested = hikes.filter(hike =>
-					hike.country === stats.favouriteCountry &&
-					hike.province === stats.favouriteProvince
-				);
+					suggested = hikes.filter(hike =>
+						hike.country === stats.favouriteCountry &&
+						hike.province === stats.favouriteProvince
+					);
 
-				if (suggested.length > 0) hikes = suggested;
+					if (suggested.length > 0) hikes = suggested;
 
-				suggested = hikes.filter(hike =>
-					hike.expectedTime >= stats.minTime &&
-					hike.expectedTime <= stats.maxtime
-				);
+					suggested = hikes.filter(hike =>
+						hike.expectedTime >= stats.minTime &&
+						hike.expectedTime <= stats.maxtime
+					);
 
-				if (suggested.length > 0) hikes = suggested;
+					if (suggested.length > 0) hikes = suggested;
 
-				suggested = hikes.filter(hike =>
-					hike.ascent >= stats.minAscent &&
-					hike.ascent <= stats.maxAscent
-				);
+					suggested = hikes.filter(hike =>
+						hike.ascent >= stats.minAscent &&
+						hike.ascent <= stats.maxAscent
+					);
 
-				if (suggested.length > 0) hikes = suggested;
+					if (suggested.length > 0) hikes = suggested;
 
-				suggested = hikes.filter(hike =>
-					hike.length >= stats.minDistance &&
-					hike.length <= stats.maxDistance
-				);
+					suggested = hikes.filter(hike =>
+						hike.length >= stats.minDistance &&
+						hike.length <= stats.maxDistance
+					);
 
-				if (suggested.length > 0) hikes = suggested;
+					if (suggested.length > 0) hikes = suggested;
 
-				setHikes(hikes);
-				setFilteredHikes(hikes);
+
+					setHikes(hikes);
+					setUserStats(stats);
+				}
+
 				setLoading(false);
 			})
 			.catch((error) => { console.log(error); })
@@ -73,14 +76,29 @@ export function HomePage(props) {
 				<Loading />
 			) : (
 				<Container>
-					<h1 className="mt-3 text-center">Our Suggestions For You</h1>
-					<HikeListTable
-						hikes={filteredHikes}
-						suggested={true}
-						selectedHike={selectedHike}
-						setSelectedHike={setSelectedHike}
-						user={props.user}
-					/>
+					{!userStats ?
+						(
+								<Container className="mt-5">
+									<Row className="d-flex justify-content-center text-center">
+										Sorry there are no suggestions for now <br />
+									</Row>
+									<Row className="d-flex justify-content-center text-muted mt-2">
+										Go and expore the world!
+									</Row>
+								</Container>
+						) : (
+							<>
+								<h1 className="mt-3 text-center">Our Suggestions For You</h1>
+								<HikeListTable
+									hikes={hikes}
+									suggested={true}
+									selectedHike={selectedHike}
+									setSelectedHike={setSelectedHike}
+									user={props.user}
+								/>
+							</>
+						)
+					}
 				</Container>
 			)}
 		</>
