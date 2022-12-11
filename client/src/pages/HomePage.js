f10c8dc294b2e1dfab610639bee8fb1fa4d90a72
LurkingNemo <giovanniriela99@gmail.com>
2022-12-11 18:17:34 +0100

    fixed bug in homepage :bug:

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -14,7 +14,14 @@ export function HomePage(props) {
 	const [selectedHike, setSelectedHike] = useState(null);
 
 	const getSuggestedHikes = async () => {
-		let stats = await UserAPI.getUserStats();
+		let stats;
+		try {
+			stats = await UserAPI.getUserStats();
+		}
+		catch (e) {
+			stats = null;
+		}
+
 		let hikes
 		await HikeAPI.getAllHikes()
 			.then(h => {
@@ -78,14 +85,14 @@ export function HomePage(props) {
 				<Container>
 					{!userStats ?
 						(
-								<Container className="mt-5">
-									<Row className="d-flex justify-content-center text-center">
-										Sorry there are no suggestions for now <br />
-									</Row>
-									<Row className="d-flex justify-content-center text-muted mt-2">
-										Go and expore the world!
-									</Row>
-								</Container>
+							<Container className="mt-5">
+								<Row className="d-flex justify-content-center text-center">
+									Sorry there are no suggestions for now <br />
+								</Row>
+								<Row className="d-flex justify-content-center text-muted mt-2">
+									Go and expore the world!
+								</Row>
+							</Container>
 						) : (
 							<>
 								<h1 className="mt-3 text-center">Our Suggestions For You</h1>
