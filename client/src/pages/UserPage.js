cfddffd550942e431133e8cc2d752e800b22ac22
Antonio Campobasso <campobassoantonio7@gmail.com>
2022-12-12 01:08:33 +0100

    preference form finished

--- client/src/pages/UserPage.js
+++ client/src/pages/UserPage.js
@@ -1,6 +1,11 @@
-import { faClock, faCompass, faHiking, faMap, faMountain } from "@fortawesome/free-solid-svg-icons";
+import {
+	faClock,
+	faCompass,
+	faHiking,
+	faMap,
+	faMountain,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Avatar } from "@mui/material";
 import { useEffect, useState } from "react";
 import {
 	Container,
@@ -10,9 +15,12 @@ import {
 	Button,
 	Modal,
 	Form,
+	InputGroup,
 } from "react-bootstrap";
 import { Navigate } from "react-router-dom";
 import UserAPI from "../api/UserAPI";
+
+import { CountrySelect, ProvinceSelect } from "../components/CoMunProvSelect";
 
 import "../styles/UserPage.css";
 
@@ -33,10 +41,11 @@ export function UserPage(props) {
 	};
 
 	useEffect(() => {
-		getStats().then((stats) => {
-			setStats(stats);
-		});
-	}, []);
+		if(!showPreferenceForm)
+			getStats().then((stats) => {
+				setStats(stats);
+			});
+	}, [showPreferenceForm]);
 
 	return props.user ? (
 		<>
@@ -46,7 +55,8 @@ export function UserPage(props) {
 				handleOpenPreferenceForm={handleOpenPreferenceForm}
 			/>
 			<PreferenceForm
-				stats={props.stats}
+				stats={stats}
+				setStats={setStats}
 				user={props.user}
 				showPreferenceForm={showPreferenceForm}
 				onHide={handleClosePreferenceForm}
@@ -58,14 +68,48 @@ export function UserPage(props) {
 }
 
 function PreferenceForm(props) {
+	const [completedHikes, setCompletedHikes] = useState(
+		props.stats.completedHikes ? props.stats.completedHikes : 0
+	);
+	const [favoriteDifficulty, setFavoriteDifficulty] = useState(
+		props.stats.favoriteDifficulty ? props.stats.favoriteDifficulty : ""
+	);
 
-	const handleSubmit = () => {
+	const [favouriteCountry, setFavoriteCountry] = useState(
+		props.stats.favouriteCountry ? props.stats.favouriteCountry : ""
+	);
+	const [favoriteProvince, setFavoriteProvince] = useState(
+		props.stats.favoriteProvince ? props.stats.favoriteProvince : ""
+	);
+
+	const [minDistance, setMinDistance] = useState(props.stats.minDistance ? props.stats.minDistance : 0);
+	const [maxDistance, setMaxDistance] = useState(props.stats.maxDistance ? props.stats.maxDistance : 0);
+
+	const [minAscent, setMinAscent] = useState(props.stats.minAscent ? props.stats.minAscent : 0);
+	const [maxAscent, setMaxAscent] = useState(props.stats.maxAscent ? props.stats.maxAscent : 0);
+
+	const [minTime, setMinTime] = useState(props.stats.minTime ? props.stats.minTime : 0);
+	const [maxTime, setMaxTime] = useState(props.stats.maxTime ? props.stats.maxTime : 0);
+
+	const handleSubmit = (ev) => {
+		ev.preventDefault();
+
 		let stats = {
-			minAscent: 0,
+			completedHikes: completedHikes,
+			favoriteDifficulty: favoriteDifficulty,
+			favouriteCountry: favouriteCountry,
+			favoriteProvince: favoriteProvince,
+			minDistance: minDistance,
+			maxDistance: maxDistance,
+			minAscent: minAscent,
+			maxAscent: minAscent,
+			minTime: minTime,
+			maxTime: maxTime,
 		};
 
 		UserAPI.setUserStats(stats);
-	} 
+		props.setStats(stats);
+	};
 
 	return (
 		<Modal show={props.showPreferenceForm} onHide={props.onHide}>
@@ -76,14 +120,29 @@ function PreferenceForm(props) {
 						<Row>
 							<Col>
 								<Form.Label>Completed Hikes</Form.Label>
-								<Form.Control />
+								<Form.Control
+									type="number"
+									value={completedHikes}
+									onChange={(ev) => setCompletedHikes(ev.target.value)}
+								/>
 							</Col>
-							<Col xs={2} className="d-flex align-items-center justify-content-center">
-							<FontAwesomeIcon icon={faCompass} size="xl"/>
+							<Col
+								xs={2}
+								className="d-flex align-items-center justify-content-center"
+							>
+								<FontAwesomeIcon icon={faCompass} size="xl" />
 							</Col>
 							<Col>
 								<Form.Label>Favorite Difficulty</Form.Label>
-								<Form.Control />
+								<Form.Select
+									value={favoriteDifficulty}
+									onChange={(ev) => setFavoriteDifficulty(ev.target.value)}
+								>
+									<option>Select difficulty</option>
+									<option value="Tourist">Tourist</option>
+									<option value="Hiker">Hiker</option>
+									<option value="Pro Hiker">Pro Hiker</option>
+								</Form.Select>
 							</Col>
 						</Row>
 					</Form.Group>
@@ -92,14 +151,25 @@ function PreferenceForm(props) {
 						<Row>
 							<Col>
 								<Form.Label>Favorite Country</Form.Label>
-								<Form.Control />
+								<CountrySelect
+									country={favouriteCountry}
+									setCountry={setFavoriteCountry}
+								/>
 							</Col>
-							<Col xs={2} className="d-flex align-items-center justify-content-center">
-							<FontAwesomeIcon icon={faMap} size="xl"/>
+							<Col
+								xs={2}
+								className="d-flex align-items-center justify-content-center"
+							>
+								<FontAwesomeIcon icon={faMap} size="xl" />
 							</Col>
 							<Col>
 								<Form.Label>Favorite Province</Form.Label>
-								<Form.Control />
+								<ProvinceSelect
+									disabled={favouriteCountry === ""}
+									province={favoriteProvince}
+									setProvince={setFavoriteProvince}
+									country={favouriteCountry}
+								/>
 							</Col>
 						</Row>
 					</Form.Group>
@@ -108,48 +178,103 @@ function PreferenceForm(props) {
 						<Row>
 							<Col>
 								<Form.Label>Minimum Distance</Form.Label>
-								<Form.Control />
+								<InputGroup>
+									<Form.Control
+										type="number"
+										value={minDistance}
+										onChange={(ev) => setMinDistance(ev.target.value)}
+									/>
+									<InputGroup.Text>meters</InputGroup.Text>
+								</InputGroup>
 							</Col>
-							<Col xs={2} className="d-flex align-items-center justify-content-center">
-							<FontAwesomeIcon icon={faHiking} size="xl"/>
+							<Col
+								xs={2}
+								className="d-flex align-items-center justify-content-center"
+							>
+								<FontAwesomeIcon icon={faHiking} size="xl" />
 							</Col>
 							<Col>
 								<Form.Label>Max Distance</Form.Label>
-								<Form.Control />
+								<InputGroup>
+									<Form.Control
+										type="number"
+										value={maxDistance}
+										onChange={(ev) => setMaxDistance(ev.target.value)}
+									/>
+									<InputGroup.Text>meters</InputGroup.Text>
+								</InputGroup>
 							</Col>
 						</Row>
 						<Row>
 							<Col>
 								<Form.Label>Minimum Ascent</Form.Label>
-								<Form.Control />
+								<InputGroup>
+									<Form.Control
+										type="number"
+										value={minAscent}
+										onChange={(ev) => setMinAscent(ev.target.value)}
+									/>
+									<InputGroup.Text>meters</InputGroup.Text>
+								</InputGroup>
 							</Col>
-							<Col xs={2} className="d-flex align-items-center justify-content-center">
-							<FontAwesomeIcon icon={faMountain} size="xl"/>
+							<Col
+								xs={2}
+								className="d-flex align-items-center justify-content-center"
+							>
+								<FontAwesomeIcon icon={faMountain} size="xl" />
 							</Col>
 							<Col>
 								<Form.Label>Max Ascent</Form.Label>
-								<Form.Control />
+								<InputGroup>
+									<Form.Control
+										type="number"
+										value={maxAscent}
+										onChange={(ev) => setMaxAscent(ev.target.value)}
+									/>
+									<InputGroup.Text>meters</InputGroup.Text>
+								</InputGroup>
 							</Col>
 						</Row>
 						<Row>
 							<Col>
 								<Form.Label>Minimum Time</Form.Label>
-								<Form.Control />
+								<InputGroup>
+									<Form.Control
+										type="number"
+										value={minTime}
+										onChange={(ev) => setMinTime(ev.target.value)}
+									/>
+									<InputGroup.Text>minutes</InputGroup.Text>
+								</InputGroup>
 							</Col>
-							<Col xs={2} className="d-flex align-items-center justify-content-center">
-							<FontAwesomeIcon icon={faClock} size="xl"/>
+							<Col
+								xs={2}
+								className="d-flex align-items-center justify-content-center"
+							>
+								<FontAwesomeIcon icon={faClock} size="xl" />
 							</Col>
 							<Col>
 								<Form.Label>Max Time</Form.Label>
-								<Form.Control />
+								<InputGroup>
+									<Form.Control
+										type="number"
+										value={maxTime}
+										onChange={(ev) => setMaxTime(ev.target.value)}
+									/>
+									<InputGroup.Text>minutes</InputGroup.Text>
+								</InputGroup>
 							</Col>
 						</Row>
 					</Form.Group>
 				</Form>
 			</Modal.Body>
 			<Modal.Footer>
-				<Button onClick={props.onHide} variant="secondary">Cancel</Button>{" "}
-				<Button type="submit" onClick={handleSubmit} variant="success">Set</Button>
+				<Button onClick={props.onHide} variant="secondary">
+					Cancel
+				</Button>{" "}
+				<Button type="submit" onClick={handleSubmit} variant="success">
+					Set
+				</Button>
 			</Modal.Footer>
 		</Modal>
 	);
