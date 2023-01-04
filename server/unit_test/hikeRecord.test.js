4ec8ef5dae2827515033e7bf2d117f4d20aa9bce
Edoardo Colella <edoardocolella.it@gmail.com>
2023-01-04 18:53:27 +0100

    unit tests on hike records (end and get)

--- server/unit_test/hikeRecord.test.js
+++ server/unit_test/hikeRecord.test.js
@@ -16,7 +16,7 @@ describe("HikeRecords test suite", () => {
 
 
     describe("Start an hike", () => {
-        test("successful start of an hike", async () => {
+        test("successful start of an hike and get", async () => {
             let creatorUser, user
             await userController.register({
                 name: 'matteo', surname: 'marroni', email: 'matteo.marroni@ex.com', phoneNumber: '2222222222',
@@ -54,12 +54,15 @@ describe("HikeRecords test suite", () => {
 
             await hikeRecordsController.addNewRecord(toBeInsertedRecord)
 
-            const records = await hikeRecordsDAO.getRecords(user.userID);
-            expect(records[0].userID).toBe(toBeInsertedRecord.userID)
-            expect(records[0].hikeID).toBe(toBeInsertedRecord.hikeID)
-            expect(records[0].status).toBe("open")
-            expect(dayjs(records[0].startDate).format()).toBe(toBeInsertedRecord.startDate)
-            expect(records[0].endDate).toBe(null)
+            const record = await hikeRecordsController.getRecordByStatusOpen(user.userID);
+
+            expect(record.userID).toBe(toBeInsertedRecord.userID)
+            expect(record.hikeID).toBe(toBeInsertedRecord.hikeID)
+            expect(record.status).toBe("open")
+            expect(dayjs(record.startDate).format()).toBe(toBeInsertedRecord.startDate)
+            expect(record.endDate).toBe(null)
+
+
 
         })
 
@@ -98,48 +101,68 @@ describe("HikeRecords test suite", () => {
                 startDate: "invalid",
             }
 
-            let newErr;
-            await hikeRecordsController.addNewRecord(toBeInsertedRecord)
-                .catch(err => newErr = err);
+        })
 
-            expect(newErr).not.toBeUndefined()
+    })
+
+    describe("Terminate an hike", () => {
+        test("Succesful end of an hike an get", async () => {
+            let creatorUser, user
+            await userController.register({
+                name: 'matteo', surname: 'marroni', email: 'matteo.marroni@ex.com', phoneNumber: '2222222222',
+                type: "localGuide", password: crypto.randomBytes(16).toString("hex")
+            }, 1, 1)
+                .then(u => {
+                    creatorUser = u;
+                })
+                .catch(err => { console.error(err); throw err; });
+
+            await userController.register({
+                name: 'marco', surname: 'verdi', email: 'marco.verdi@ex.com', phoneNumber: '1111111111',
+                type: "hiker", password: crypto.randomBytes(16).toString("hex")
+            }, 1, 1)
+                .then(u => {
+                    user = u;
+                })
+                .catch(err => { console.error(err); throw err; });
+
+            let newErr
+            let hike = {
+                title: "title", length: 10, expectedTime: 10, ascent: 10,
+                difficulty: "Hiker", description: "Description", country: "Italy",
+                municipality: "Torino", province: "Torino", track: [[10, 10], [11, 11]], creatorID: creatorUser.userID
+            }
+            await hikeController.addHike(hike)
+                .catch(err => newErr = err)
+
+            let startDate = dayjs('2018-04-13 19:18').format()
+            let endDate = dayjs('2018-04-14 19:18').format()
+
+            let toBeInsertedRecord = {
+                userID: user.userID,
+                hikeID: 1,
+                startDate: startDate,
+            }
+
+            await hikeRecordsController.addNewRecord(toBeInsertedRecord)
+            await hikeRecordsController.editRecord(
+                {
+                    userID: user.userID,
+                    hikeID: 1,
+                    startDate: startDate,
+                    endDate: endDate
+                }
+            )
+
+            const records = await hikeRecordsDAO.getRecords(user.userID)
+            expect(records[0].userID).toBe(user.userID)
+            expect(records[0].hikeID).toBe(1)
+            expect(records[0].startDate).toBe(startDate)
+            expect(records[0].endDate).toBe(endDate)
+            expect(records[0].status).toBe("completed")
 
         })
 
     })
-    /* 
-        describe("Terminate an hike", () => {
-            test("", async () => {
-                await hikeRecordsController.editRecord(
-                    {
-                        userID: ,
-                        hikeID: ,
-                        status: ,
-                        startDate: ,
-                        endDate: null
-                    }
-                )
-            })
-            test("", async () => {
-    
-            })
-        })
-    
-        describe("Get all hikes", () => {
-            test("", async () => {
-                await hikeRecordsController.getRecords()
-            })
-            test("", async () => {
-    
-            })
-        })
-    
-        describe("Get all open hikes", () => {
-            test("", async () => {
-                await hikeRecordsController.getRecordByStatusOpen()
-            })
-            test("", async () => {
-    
-            })
-        }) */
+
 })
