c8f317254f66e21345c478523c8cba43ead3184a
antonio <campobassoantonio7@gmail.com>
2023-01-07 22:39:35 +0100

    completed hikes

--- client/src/components/Profile/CompletedHikesList.js
+++ client/src/components/Profile/CompletedHikesList.js
@@ -1,32 +1,54 @@
 import { useEffect, useState } from "react";
+import { ListGroup } from "react-bootstrap";
+import HikeAPI from "../../api/HikeAPI";
 import HikeRecordsAPI from "../../api/HikeRecordsAPI";
+import { Loading } from "../Loading";
 
 export function CompletedHikesList(props) {
-	//let [recordList, setRecordList] = useState([]);
-
-	let [recordMap, setRecordMap] = useState(new Map());
+	let [hikeRecordList, setHikeRecordList] = useState([]);
+	let [loading, setLoading] = useState(true);
 
 	useEffect(() => {
 		HikeRecordsAPI.getHikeRecordsForUser(props.user.userID).then((list) => {
-			let tempRecordMap = new Map();
-			
+			let recordMap = new Map();
+			let tempRecordList = new Array();
+
 			list.forEach((record) => {
-				if(tempRecordMap.has(record.hikeID)){
-					let oldValue = tempRecordMap.get(record.hikeID);
+				if (recordMap.has(record.hikeID)) {
+					let oldValue = recordMap.get(record.hikeID);
 
-					tempRecordMap.set(record.hikeID, oldValue.concat({start: record.start, end: record.end}));
+					recordMap.set(
+						record.hikeID,
+						oldValue.concat({ start: record.startDate, end: record.endDate })
+					);
 				} else {
-					tempRecordMap.set(record.hikeID, [{start: record.start, end: record.end}])
+					recordMap.set(record.hikeID, [
+						{ start: record.startDate, end: record.endDate },
+					]);
 				}
-			})
+			});
 
-			setRecordMap(tempRecordMap);
-			console.log(tempRecordMap);
+			recordMap.forEach((dates, hikeID) => {
+				HikeAPI.getHike(hikeID).then((hike) => {
+					tempRecordList.push({ ...hike, dates: dates });
+				});
+			});
 
-			
+			setHikeRecordList(tempRecordList);
+			setLoading(false);
 		});
 	}, []);
-	
+
+	return loading ? (
+		<Loading />
+	) : (
+		<ListGroup variant="flush">
+			{hikeRecordList.map((hikeRecord) => (
+				<ListGroup.Item key={hikeRecord.hikeID}>aaa</ListGroup.Item>
+			))}
+		</ListGroup>
+	);
+
 	// map list to dict of unique id - array of start/end
 
 	// get info of all hikeID present
@@ -34,5 +56,4 @@ export function CompletedHikesList(props) {
 	// display map along with basic info
 
 	// display array of start end somehow
-
 }
