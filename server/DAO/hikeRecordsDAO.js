536941606f9301b3f2b87b75dd79f4efadd60510
LucaBasso <s258211@studenti.polito.it>
2023-01-08 16:46:34 +0100

    Added tests and open hikes check

--- server/DAO/hikeRecordsDAO.js
+++ server/DAO/hikeRecordsDAO.js
@@ -6,17 +6,17 @@ const db = dbManager.getDB();
 
 
 exports.getRecords = async (userID) => {
-    return new Promise((resolve, reject) => {
+	return new Promise((resolve, reject) => {
 		const sql = "SELECT * FROM USERHIKERECORDS WHERE userID = ?";
 		db.all(sql, [userID], function (err, rows) {
-            if (err) reject(err);
+			if (err) reject(err);
 			resolve(rows);
 		});
 	});
 }
 
 exports.getRecordWithStatusOpen = async (userID) => {
-    return new Promise((resolve, reject) => {
+	return new Promise((resolve, reject) => {
 		const sql = "SELECT * FROM USERHIKERECORDS WHERE userID = ? AND status = 'open' ";
 		db.get(sql, [userID], function (err, row) {
 			if (err) reject(err);
@@ -26,8 +26,12 @@ exports.getRecordWithStatusOpen = async (userID) => {
 }
 
 exports.addNewRecord = async (record) => {
-    const {userID, hikeID, status, startDate, endDate} = record
-    return new Promise((resolve, reject) => {
+	const {userID, hikeID, status, startDate, endDate} = record
+	return new Promise(async (resolve, reject) => {
+		let openRecord= await this.getRecordWithStatusOpen(userID)
+			.catch(err=>reject(err));
+		if(openRecord!=null)
+			reject("No more than one hike can be open at the same time");
 		const sql = "INSERT INTO USERHIKERECORDS (userID, hikeID, status, startDate, endDate) VALUES(?,?,?,?,?)";
 		db.run(sql, [userID, hikeID, status, startDate, endDate], function (err, row) {
 			if (err) reject(err);
@@ -38,10 +42,10 @@ exports.addNewRecord = async (record) => {
 
 exports.editRecord = async (record) => {
 	const { userID, hikeID, status, startDate, endDate } = record
-    return new Promise((resolve, reject) => {
-        const sql = `UPDATE USERHIKERECORDS
-                    SET status=?, endDate = ?
-                    WHERE userID = ? AND hikeID = ? AND startDate = ?`;
+	return new Promise((resolve, reject) => {
+		const sql = `UPDATE USERHIKERECORDS
+					SET status=?, endDate = ?
+					WHERE userID = ? AND hikeID = ? AND startDate = ?`;
 		db.run(sql, [status, endDate, userID, hikeID, startDate], function (err, row) {
 			if (err) reject(err);
 			resolve();
